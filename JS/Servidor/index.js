63e5811953ffb49cdb703f768e09bfefbec843b9
LINDEMBERGSON CARLOS MONTEIRO MORENO <20232ADSGR0322@labs.garanhuns.ifpe>
2025-07-31 20:44:23 -0300

    servidor

--- JS/Servidor/index.js
+++ JS/Servidor/index.js
@@ -1,3 +1,10 @@
+import express from "express"
+
+const app = express();
+app.use(express.static("public"))
 
 
-
+app.listen(8080,ola)
+function ola(){
+    console.log("Aplicação aberta na porta 8080")
+}
